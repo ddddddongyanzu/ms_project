f9c7a5bece77442afbf300c67691be0d8b491b02
ddddddongyanzu <94530940+ddddddongyanzu@users.noreply.github.com>
2025-01-14 19:02:09 +0800

    ELK_COMPLETE

--- project-common/kk/kafka_test.go
+++ project-common/kk/kafka_test.go
@@ -9,17 +9,17 @@ import (
 func TestProducer(t *testing.T) {
 	w := GetWriter("localhost:9092")
 	m := make(map[string]string)
-	m["projectCode"] = "1100"
+	m["projectCode"] = "1500"
 	bytes, _ := json.Marshal(m)
 	w.Send(LogData{
-		Topic: "log",
+		Topic: "msproject_log",
 		Data:  bytes,
 	})
 	time.Sleep(2 * time.Second)
 }
 
 func TestConsumer(t *testing.T) {
-	GetReader([]string{"localhost:9092"}, "group1", "log")
+	GetReader([]string{"localhost:9092"}, "group1", "msproject_log")
 	for {
 
 	}
